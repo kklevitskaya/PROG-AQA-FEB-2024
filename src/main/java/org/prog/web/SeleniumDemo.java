ab3a92abe299cb89b295a75b4d4d351e8cb85cdd
Ekaterina Klevitskaya <eklevitskaya@mail.ru>
2024-02-29 18:41:09 +0300

    Selenium find element

--- src/main/java/org/prog/web/SeleniumDemo.java
+++ src/main/java/org/prog/web/SeleniumDemo.java
@@ -2,6 +2,7 @@ package org.prog.web;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumDemo {
@@ -9,7 +10,10 @@ public class SeleniumDemo {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         try {
-            driver.get("https://google.com/");
+            driver.get("https://rozetka.com.ua");
+
+            WebElement elementByXPath = driver.findElement(By.xpath("/html/body/app-root/div/div/rz-header/rz-main-header/header/div/div/a/picture/img"));
+
         } finally {
             driver.quit();
         }
