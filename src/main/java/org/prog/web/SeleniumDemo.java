9641d9d387c9bb032fb311f02fc405c49922b93c
kklevitskaya <sinicaby00@gmail.com>
2024-03-05 10:44:11 +0100

    Search title by get.Attribute

--- src/main/java/org/prog/web/SeleniumDemo.java
+++ src/main/java/org/prog/web/SeleniumDemo.java
@@ -19,43 +19,64 @@ public class SeleniumDemo {
 
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+//        try {
+//            driver.get("https://google.com/");
+//            //accept cookies if they are present
+//            //find all buttons in cookies form
+//            List<WebElement> cookieButtons =
+//                    driver.findElements(By.xpath(
+//                            "//a[contains(@href,'https://policies.google.com/technologies/cookies')]/../../../..//button"));
+//            if (!cookieButtons.isEmpty()) {
+//                //click button with index 3 (this button accepts all cookies)
+//                cookieButtons.get(3).click();
+//            }
+//            //TODO: sreach for ben Affleck
+//            //Step 1: set search form value to Ben Affleck
+//            WebElement searchInput = driver.findElement(By.name("q"));
+//            searchInput.sendKeys("Ben Affleck");
+//            //Step 2: press enter while in search form
+//            searchInput.sendKeys(Keys.ENTER);
+//
+//            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30L));
+//            List<WebElement> searchHeaders =
+//                    wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.tagName("h3"), 5));
+//
+//            System.out.println(">>>>> " + searchHeaders.size());
+//
+//            int afflecksFound = 0;
+//            for (WebElement element : searchHeaders) {
+//                if (element.getText().contains("Ben Affleck")) {
+//                    afflecksFound++;
+//                }
+//            }
+//            if (afflecksFound >= 3) {
+//                System.out.println("Ben found!");
+//            } else {
+//                System.out.println("Ben not found :(");
+//            }
+//        } finally {
+//            driver.quit();
+//        }
+//    }
         try {
-            driver.get("https://google.com/");
-            //accept cookies if they are present
-            //find all buttons in cookies form
-            List<WebElement> cookieButtons =
-                    driver.findElements(By.xpath(
-                            "//a[contains(@href,'https://policies.google.com/technologies/cookies')]/../../../..//button"));
-            if (!cookieButtons.isEmpty()) {
-                //click button with index 3 (this button accepts all cookies)
-                cookieButtons.get(3).click();
-            }
-            //TODO: sreach for ben Affleck
-            //Step 1: set search form value to Ben Affleck
-            WebElement searchInput = driver.findElement(By.name("q"));
-            searchInput.sendKeys("Ben Affleck");
-            //Step 2: press enter while in search form
+            driver.get("https://allo.ua/");
+
+            WebElement searchInput = driver.findElement(By.name("search"));
+            searchInput.sendKeys("iPhone 15");
             searchInput.sendKeys(Keys.ENTER);
 
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30L));
-            List<WebElement> searchHeaders =
-                    wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.tagName("h3"), 5));
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3L));
+            WebElement firstGridItem = wait.until(ExpectedConditions.
+                    visibilityOfElementLocated
+                            (By.xpath("//*[@id=\"__layout\"]/div/div[1]/div[2]/div/div[2]/div[2]/div[1]/div/div[3]/a")));
 
-            System.out.println(">>>>> " + searchHeaders.size());
+            String productTitle = firstGridItem.getAttribute("title");
+            System.out.println(productTitle);
 
-            int afflecksFound = 0;
-            for (WebElement element : searchHeaders) {
-                if (element.getText().contains("Ben Affleck")) {
-                    afflecksFound++;
-                }
-            }
-            if (afflecksFound >= 3) {
-                System.out.println("Ben found!");
-            } else {
-                System.out.println("Ben not found :(");
-            }
         } finally {
             driver.quit();
         }
+
+
     }
 }
